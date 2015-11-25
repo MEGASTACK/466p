edcd80afee95c7f13f21167837730a4df2596163
John Slevinsky <john.slev@gmail.com>
2015-11-25 14:43:31 -0700

    ... on the road to out-of-the-box example working...

--- main.m
+++ main.m
@@ -17,7 +17,34 @@ addpath JustinsGraphicalModelsToolboxPublic
 % loss_spec: 
 
 
-feats = {};
+feat_params = {{'patches',0},{'position',1},{'fourier',1},{'hog',8}};
+
+
+fprintf('loading data and computing feature maps...\n');
+N = 1;
+for n=1:N
+    % load data
+%     lab = importdata([labdir lab_names(n).name]);
+    lab = max_press;
+    im  = double(max_press)/255;
+    ims{n}  = im;
+    labels0{n} = max(0,lab+1);
+
+    % compute features
+    feats{n}  = featurize_im(ims{n},feat_params);
+
+    % reduce resolution for speed
+%     ims{n}    = imresize(ims{n}   ,rez,'bilinear');
+%     feats{n}  = imresize(feats{n} ,rez,'bilinear');
+%     labels{n} = imresize(labels0{n},rez,'nearest');
+
+    % reshape features
+    [ly lx lz] = size(feats{n});
+    feats{n} = reshape(feats{n},ly*lx,lz);
+end
+
+
+
 efeats = [];
 labels = [];
 models = [];
