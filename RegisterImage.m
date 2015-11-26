4ebf546ff4b1f7293c40a201c66f70d67003a2fd
IrisAmp <brandon.d.yue@gmail.com>
2015-11-26 10:06:22 -0700

    Added in and commented out lines to show before and after images of registration

--- RegisterImage.m
+++ RegisterImage.m
@@ -11,10 +11,11 @@ MaxValue = max(max([Target, Reference]));
 RefIm = mat2gray(Reference, [0, MaxValue]);
 TarIm = mat2gray(Target,    [0, MaxValue]);
 
+%imshowpair(RefIm, TarIm, 'diff');
+%pause
+
 % Perform registration
-% Monomodal, because the two images should have about the same intensity
-% ranges.
-[optimizer, metric] = imregconfig('monomodal');
+[optimizer, metric] = imregconfig('multimodal');
 % Notes from Mathworks:
 % Your registration results can improve if you adjust the optimizer or 
 % metric settings. For example, if you increase the number of iterations in
@@ -23,6 +24,8 @@ TarIm = mat2gray(Target,    [0, MaxValue]);
 % the expense of performance.
 %optimizer.MaximumIterations = 300;
 Output = imregister(TarIm, RefIm, 'rigid', optimizer, metric);
+
+%imshowpair(RefIm, Output, 'diff');
 
 % The output is normalized to [0, 1], so first we can re-scale it back to
 % its original format.
