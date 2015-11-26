1b4a6a4e464ebf459adb662ef440d11422b65d92
IrisAmp <brandon.d.yue@gmail.com>
2015-11-26 09:47:21 -0700

    Minor cleanup

--- RegisterImage.m
+++ RegisterImage.m
@@ -1,6 +1,6 @@
 function [ Output ] = RegisterImage( Reference, Target )
 %% RegisterImage
-% Registers (aligns) the input image (Target) agains the given Reference
+% Registers (aligns) the input image (Target) against the given Reference
 % image. Requires/uses the MATLAB Image Processing Toolbox.
 
 % Convert the input matricies to grayscale image formats.
@@ -12,7 +12,16 @@ RefIm = mat2gray(Reference, [0, MaxValue]);
 TarIm = mat2gray(Target,    [0, MaxValue]);
 
 % Perform registration
-[optimizer, metric] = imregconfig('multimodal');
+% Monomodal, because the two images should have about the same intensity
+% ranges.
+[optimizer, metric] = imregconfig('monomodal');
+% Notes from Mathworks:
+% Your registration results can improve if you adjust the optimizer or 
+% metric settings. For example, if you increase the number of iterations in
+% the optimizer, reduce the optimizer step size, or change the number of 
+% samples in a stochastic metric, the registration improves to a point, at 
+% the expense of performance.
+%optimizer.MaximumIterations = 300;
 Output = imregister(TarIm, RefIm, 'rigid', optimizer, metric);
 
 % The output is normalized to [0, 1], so first we can re-scale it back to
