9c1481655a889eccd3aac8e051a0136cdc601647
Shadi Merhej <Shadi Merhej>
2015-12-01 17:26:30 -0700

    Labels for NP40/3110

--- FinalData/NP40/Labeller.m
+++ FinalData/NP40/Labeller.m
@@ -1,74 +1,67 @@
-function [label_3108] = Labeller(filename);
+function [label_3110] = Labeller(filename);
 
 
 [~,pedo_max,~,~,~] = pedo_extract(filename);
-label_3108 = zeros(size(pedo_max));
+label_3110 = zeros(size(pedo_max));
 %BigToe
 for i=1:9
     for j=1:9
         if pedo_max(i,j)>0
-            label_3108(i,j)=1;
+            label_3110(i,j)=1;
         end
     end
 end
 
 %Medial
-k=0
-for i=11:22
-    for j=1:12-k
+for i=11:14
+    for j=1:11
         if pedo_max(i,j)>0
-            label_3108(i,j)=2;
-        if k<3
-            k=k+1;
+            label_3110(i,j)=2;
         end
     end
 end
-
+for i=15:21
+    for j=1:10
+        if pedo_max(i,j)>0
+            label_3110(i,j)=2;
+        end
+    end
+end
 %Lateral
 k=0;
-r=0;
-for i=13:22
-     if i>14
-        r=r+1;
-     end
-     if i>17
-        r=r+1;
-     end
-     if i>21
-        r=r+1;
-     end
-    for j=(13-r):(14+k)
-        if j==-1 | j==0
-            j=19;
-        end
-        if pedo_max(i,j)>0
-            label_3108(i,j)=3;
-            if k<5
-                k=k+1;
-            end           
-        end
+for i=11:14
+    for j=12:(13+k)
+       if k<6
+         k=k+1;
+       end  
+       if pedo_max(i,j)>0
+          label_3110(i,j)=3;   
+       end       
     end
 end
 
 %Lateral part 2
-for i=23:26
-    for j=11:size(label_3108,2)
-        if pedo_max(i,j)>0
-            label_3108(i,j)=3;
-        end
+for i=15:23
+    for j=11:(13+k)
+       if k<6
+         k=k+1;
+       end  
+       if pedo_max(i,j)>0
+          label_3110(i,j)=3;   
+       end       
     end
 end
 
 %Heel
-for i=40:size(label_3108,1)
-    for j=1:size(label_3108,2)
+for i=41:size(label_3110,1)
+    for j=1:size(label_3110,2)
         if pedo_max(i,j)>0
-            label_3108(i,j)=4;
+            label_3110(i,j)=4;
         end
     end
 end
 
-disp(label_3108)
+disp(label_3110)
 end
 
 
